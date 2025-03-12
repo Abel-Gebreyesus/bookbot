6bc4276902e7f826c87a71c5d7fbd2547e20d8ae
Abel Gebreyesus <abelgebreyesus2@gmail.com>
2025-03-11 22:33:55 -0400

    Add comments

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import *
 import sys
 
+# Main - calls from stats.py and formats data
 def main():
+  # Informs user to include path to book
   if len(sys.argv) < 2:
     print('Usage: python3 main.py <path_to_book>')
     sys.exit(1)
+  # Gets path and calculates relevent information
   path = sys.argv[1]
   num = get_num_words(path)
   dict = sort_dict(get_char_instances(path))
+  # Formats information
   print('============ BOOKBOT ============')
   print('Analyzing book found at ' + path)
   print('----------- Word Count ----------')
--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
+# Finds words in txt
 def get_num_words(f):
   with open(f, 'r') as file:
     contents = file.read()
   return len(contents.split())
 
+# Finds # of instances for each char
 def get_char_instances(f):
   with open(f, 'r') as file:
     contents = file.read()
@@ -15,5 +17,6 @@ def get_char_instances(f):
       dict[char] = 1
   return dict
 
+# Sorts Dictionaries
 def sort_dict(dict):
   return sorted(dict.items(), key=lambda item: item[1], reverse=True)
